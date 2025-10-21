52b17d8d37b3d288c2d3746f42232be64b31025f
kshantigreene <kshanti@gmail.com>
2025-10-21 13:56:31 -0400

    function
    made a function

--- mvc.py
+++ mvc.py
@@ -3,12 +3,7 @@ from m import Model_List
 from c import Controller
 from v import SimpleGUI
 
-
-if __name__ == '__main__':
-    print("mvc")
-    m = Model_List() 
-    c = Controller(m)
-    v = SimpleGUI(c)
+def addItems(model):
     m.add("Buy groceries")
     m.add("Walk the dog")
     m.add("Read a book")
@@ -16,6 +11,13 @@ if __name__ == '__main__':
     m.add("Item: 1 Caleb")
     m.add("Item: 2 Cian go to work")
     m.add("Item: 3 David")
+
+if __name__ == '__main__':
+    print("mvc")
+    m = Model_List() 
+    c = Controller(m)
+    v = SimpleGUI(c)
+    addItems(m)
     v.show(m.items)
     v.run()
     m.save("./items.txt")
